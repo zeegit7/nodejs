30c9da4d6a3f11227227bc34006509b0ec44f4a2
Paul Nguyen <paul.nguyen@sjsu.edu>
2016-03-13 21:38:12 +0000

    .

--- test/api-test.js
+++ test/api-test.js
@@ -2,7 +2,7 @@
 /* REST API TEST */
 
 // Setup Rest Client 
-var endpoint = "http://ec2-52-24-214-120.us-west-2.compute.amazonaws.com/GrailsGumballMachineVer2-2.0/gumballs/1";
+var endpoint = "http://52.9.120.14:8080/GrailsGumballMachineVer2-2.0/gumballs/1";
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
