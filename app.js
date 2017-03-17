786b55c1d75800fbf979d1d2812376932e12284a
paulnguyen <paul.nguyen@sjsu.edu>
2017-03-17 04:49:24 +0000

    .

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ Serial# 1234998871109
 
 **/
 
-var endpoint = "http://52.9.120.14:8080/GrailsGumballMachineVer2-2.0/gumballs/1";
+var endpoint = "http://api.paulnguyen.org:8181/gumball/";
 
 
 // added in v2: crypto
